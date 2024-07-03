ba3feb672b048f41ae1989ea98d7cb107cce6367
26Vamsi <edaravamsi26@gmail.com>
2024-07-04 00:39:41 +0530

    Added Sidebar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Homepage from "./pages/Homepage";
+import FavoritesPage from "./pages/FavoritesPage";
+
+function App() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
