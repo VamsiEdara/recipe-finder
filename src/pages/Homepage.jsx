ab7182b0f5006180f25aa268ab6def2d2a367456
26Vamsi <edaravamsi26@gmail.com>
2024-07-04 01:26:50 +0530

    Updated files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,31 @@
 // import React from "react";
 
+import { Search } from "lucide-react";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="bg-[#faf9fb] p-10 flex-1">
+      <div className="max-w-screen-lg mx-auto">
+        <form>
+          <label className="input shadow-md flex items-center">
+            <Search size={"24"} />
+            <input
+              type="text"
+              className="text-sm md:text-md grow"
+              placeholder="What do you want to cook today ?"
+            />
+          </label>
+        </form>
+
+        <h1 className="font-bold text-3xl md:text-5xl mt-4">
+          Recommended Recipes
+        </h1>
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight ">
+          Popular choices
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
